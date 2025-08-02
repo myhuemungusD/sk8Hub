f81eece19b18fc96272289b4487e0d28f8116b69
myhuemungusD <jayham710@gmail.com>
2025-08-01 23:08:17 -0700

    Integrate Firebase config and prepare for Auth/Firestore/Storage features

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,12 +2,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_AUTH_DOMAIN',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_STORAGE_BUCKET',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID'
+  apiKey: 'AIzaSyDD62pVO8fY2Amlea-KfOH2hVrRoYQAtIA',
+  authDomain: 'sk8hub-d7806.firebaseapp.com',
+  projectId: 'sk8hub-d7806',
+  storageBucket: 'sk8hub-d7806.appspot.com',
+  messagingSenderId: '665573979824',
+  appId: '1:665573979824:android:ed510263c2a30c69ee2d75'
 };
 
 const app = initializeApp(firebaseConfig);
